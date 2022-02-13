1063d156c35d4cfb81369e6315187ad24b3658a7
MIShanto <shanto4045@gmail.com>
2022-02-13 17:38:05 +0600

    updated

---
+++ Assets/Scripts/1.NetworkManager/MyNetworkManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MyNetworkManager : NetworkManager
+{
+    [System.Obsolete]
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+
+        Debug.Log("connected..");
+
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        var player = conn.identity.GetComponent<MyNetworkPlayer>();
+        Debug.Log("player added..");
+        Debug.Log($"no player: {numPlayers}");
+
+        //change name..
+        player.SetDisplayName($"player {numPlayers}");
+
+        //change color..
+        player.SetDisplayColor(new Color(Random.Range(0f,1f), Random.Range(0f, 1f), Random.Range(0f, 1f)));
+    }
+}
---
+++ Assets/Scripts/1.NetworkManager/MyNetworkPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using System;
+
+public class MyNetworkPlayer : NetworkBehaviour
+{
+    [SyncVar] [SerializeField] string displayName = "Missing name"; // default name
+    [SyncVar(hook = nameof(HandleColor))] [SerializeField] Color displayColor = Color.white; // default color
+
+    [Server]
+    public void SetDisplayName(string newDisplayName) 
+    {
+        displayName = newDisplayName;
+    }
+
+    [Server]
+    public void SetDisplayColor(Color newdisplayColor)
+    {
+        displayColor = newdisplayColor;
+        
+    }
+
+    public void HandleColor(Color oldColor, Color newColor) // this method is called when display color variable is updated as it is hooked.. 
+    {
+        GetComponent<Renderer>().material.SetColor("_BaseColor", newColor);
+
+        //Debug.LogError(oldColor);
+        //Debug.LogError(newColor);
+    }
+}
