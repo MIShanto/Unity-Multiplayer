67f0449b825ac36afc3ffda27f07229dd808bb06
MIShanto <shanto4045@gmail.com>
2022-02-13 18:24:30 +0600

    callback added

--- Assets/Scripts/1.NetworkManager/MyNetworkPlayer.cs
+++ Assets/Scripts/1.NetworkManager/MyNetworkPlayer.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 using System;
+using TMPro;
 
 public class MyNetworkPlayer : NetworkBehaviour
 {
-    [SyncVar] [SerializeField] string displayName = "Missing name"; // default name
+    [SyncVar(hook = nameof(HandleName))] [SerializeField] string displayName = "Missing name"; // default name
     [SyncVar(hook = nameof(HandleColor))] [SerializeField] Color displayColor = Color.white; // default color
 
+    [SerializeField] Renderer renderer;
+    [SerializeField] TextMeshProUGUI nameTextUI;
+
+    #region server
     [Server]
     public void SetDisplayName(string newDisplayName) 
     {
@@ -22,11 +27,42 @@ public class MyNetworkPlayer : NetworkBehaviour
         
     }
 
+    [Command]
+    void CmdSetDisplayName(string newDisplayName) // updates name when get request from client..
+    {
+        RpcLogNewName(newDisplayName); // server calling all client.
+        
+        SetDisplayName(newDisplayName);
+
+    }
+    #endregion
+
+    #region client
     public void HandleColor(Color oldColor, Color newColor) // this method is called when display color variable is updated as it is hooked.. 
     {
-        GetComponent<Renderer>().material.SetColor("_BaseColor", newColor);
+        renderer.material.SetColor("_BaseColor", newColor);
 
         //Debug.LogError(oldColor);
         //Debug.LogError(newColor);
     }
+
+    public void HandleName(string oldName, string newName) // this method is called when display name variable is updated as it is hooked.. 
+    {
+        nameTextUI.text = newName;
+
+    }
+
+    [ContextMenu("Set new name")]
+    void UpdateDisplayName() // client asks server to update name..
+    {
+        CmdSetDisplayName("Name updated.");
+    }
+
+    [ClientRpc]
+    void RpcLogNewName(string newDisplayName) // when server calls all client..
+    {
+        Debug.LogError(newDisplayName);
+    }
+
+    #endregion
 }
